aacce7bce80f6b9c4e504bc9f01b9aeeeaf9959c
Chris <orj.tsuk@gmail.com>
2022-03-18 08:46:53 -0400

    some styling updates

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ function App() {
   return (
     <div className="App">
       <div className='nav--container'>
-        <h1>IDFK</h1>
+        <h1 className='nav--title'>IDFK</h1>
         <span className="material-icons-outlined shoppingCart">shopping_cart</span>
       </div>
       <nav
         style={{
           borderBottom: "solid 1px",
           paddingBottom: "1rem",
+          backgroundColor: "#a8dee6"
         }}
       >
         <div className='nav--links--container'>
