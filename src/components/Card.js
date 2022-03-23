b8b7e20f3d24866b5bc47136802ab4d24478ab50
Chris <orj.tsuk@gmail.com>
2022-03-23 08:45:14 -0400

    working with getting a theme set up for the items, tweaking CSS

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,13 +4,14 @@ import '../App.css'
 const Card = (props) => {
     return(
         <div className="card--container">
-            <h1 className="card--name">{props.name}</h1>
+            <h1 className="card--title">{props.name}</h1>
             <img className="card--img" src={props.image} />
             <h3 className="card--desc">{props.desc}</h3>
-            <h5 className="card--quantity">Quantity:</h5>
+            {props.onDecrease && <h5 className="card--quantity">Quantity:</h5>}
             {props.onDecrease && <button onClick={props.onDecrease} className="card--incdec">-</button>}
             {props.onDecrease && <input value={parseInt(props.quantity) || 0} onChange={props.onChange} className="card--amount" type="number"></input>}
             {props.onIncrease && <button onClick={props.onIncrease} className="card--incdec">+</button>}
+            {props.onClick && <h5>Price:</h5>}
             <h5 className="card--price">${props.price}</h5>
             {props.onClick && <button onClick={props.onClick} className="card--button">Add to cart</button>}
             {props.onRemove && <button onClick={props.onRemove}>Remove from Cart</button>}
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,4 +1,3 @@
-import { nanoid } from "nanoid";
 import React from "react";
 import Card from "./Card";
 
